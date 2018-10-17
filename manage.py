2b551541088df57446b3bb1c66db01205ecee230
mt <aa123456>
2018-10-17 18:44:53 +0800

    集成数据库迁移的命令

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -45,6 +46,10 @@ CSRFProtect(app)
 Session(app)
 
 manager = Manager(app)
+# 将app与db关联
+Migrate(app, db)
+# 将迁移命令迁移到manager中
+manager.add_command("db", MigrateCommand)
 
 
 @app.route('/')
