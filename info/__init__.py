7177c619b367ce54853d454ae19dc7cee0bb1ab3
mt <aa123456>
2018-10-17 19:10:10 +0800

    抽取app的创建到函数中&配置名字作为参数

--- info/__init__.py
+++ info/__init__.py
@@ -6,15 +6,23 @@ from redis import StrictRedis
 
 from config import config
 
-app = Flask(__name__)
-# 添加配置
-app.config.from_object(config["development"])
-
 # 初始化数据库
-db = SQLAlchemy(app)
-# 初始化redis存储对象
-redis_store = StrictRedis(host=config["development"].REDIS_HOST, port=config["development"].REDIS_PORT)
-# 开启当前项目 CSRF 保护,只做服务器验证功能
-CSRFProtect(app)
-# 设置session保存指定位置
-Session(app)
+# 在flask很多扩展里面都可以初始化扩展的对象,然后在调用init_app方法初始化
+db = SQLAlchemy()
+
+
+def creat_app(config_name):
+    app = Flask(__name__)
+    # 添加配置
+    app.config.from_object(config[config_name])
+
+    # 初始化数据库
+    db.init_app(app)
+    # 初始化redis存储对象
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+    # 开启当前项目 CSRF 保护,只做服务器验证功能
+    CSRFProtect(app)
+    # 设置session保存指定位置
+    Session(app)
+
+    return app
