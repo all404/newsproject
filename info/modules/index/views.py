f078889fa132b3705bea83278c8e3b1acf4e7358
mt <aa123456>
2018-10-18 19:41:43 +0800

    加载favicon.ico小图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from info import redis_store
 from . import index_blu
@@ -8,3 +8,8 @@ from . import index_blu
 def index():
     redis_store.set("name", "zhangsan")
     return render_template("news/index.html")
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
